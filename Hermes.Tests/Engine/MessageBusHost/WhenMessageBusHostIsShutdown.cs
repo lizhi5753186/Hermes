45a9654ff5120ac3b8692d8c529a4ca463740f4c
Tiaan de Klerk <tiaan.deklerk@gmail.com>
2016-01-20 00:11:27 +1300

    Projects

---
+++ Hermes.Tests/Engine/MessageBusHost/WhenMessageBusHostIsShutdown.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace Hermes.Tests.Engine.MessageBusHost
+{
+    [TestFixture]
+    public class WhenMessageBusHostIsShutdown : 
+        MessageBusHostTestBase
+    {
+        [OneTimeSetUp]
+        public void Scenario()
+        {
+            GivenTheMessageBusHostIsShutdown();
+        }
+
+        [Test]
+        public void ThenMessageBusHostExitCodeShouldExecute()
+        {
+            IsHostAndEngineShutdown.ShouldBe(true);
+        }
+    }
+}
