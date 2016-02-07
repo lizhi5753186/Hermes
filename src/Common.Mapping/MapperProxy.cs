c936b4a52266c72ea8a9a64afd5c9112adc846ee
Tiaan de Klerk <tiaan.deklerk@gmail.com>
2016-02-07 14:51:16 +1300

    Code cleanup

--- src/Common.Mapping/MapperProxy.cs
+++ src/Common.Mapping/MapperProxy.cs
@@ -36,54 +36,124 @@ namespace Common.Mapping
         public IConfigurationProvider ConfigurationProvider
             => _mapper.ConfigurationProvider;
 
-        public TDestination Map<TDestination>(object source)
+        public TDestination Map<TDestination>(
+            object source
+            )
         {
             return _mapper.Map<TDestination>(source);
         }
 
-        public TDestination Map<TDestination>(object source, Action<IMappingOperationOptions> opts)
+        public TDestination Map<TDestination>(
+            object source, 
+            Action<IMappingOperationOptions> opts
+            )
         {
-            return _mapper.Map<TDestination>(source, opts);
+            return _mapper.Map<TDestination>(
+                source, 
+                opts
+                );
         }
 
-        public TDestination Map<TSource, TDestination>(TSource source)
+        public TDestination Map<TSource, TDestination>(
+            TSource source
+            )
         {
             return _mapper.Map<TSource, TDestination>(source);
         }
 
-        public TDestination Map<TSource, TDestination>(TSource source, Action<IMappingOperationOptions<TSource, TDestination>> opts)
+        public TDestination Map<TSource, TDestination>(
+            TSource source, 
+            Action<IMappingOperationOptions<TSource, TDestination>> opts
+            )
         {
-            return _mapper.Map<TSource, TDestination>(source, opts);
+            return _mapper.Map<TSource, TDestination>(
+                source, 
+                opts
+                );
         }
 
-        public TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
+        public TDestination Map<TSource, TDestination>(
+            TSource source, 
+            TDestination destination
+            )
         {
-            return _mapper.Map<TSource, TDestination>(source, destination);
+            return _mapper.Map<TSource, TDestination>(
+                source, 
+                destination
+                );
         }
 
-        public TDestination Map<TSource, TDestination>(TSource source, TDestination destination, Action<IMappingOperationOptions<TSource, TDestination>> opts)
+        public TDestination Map<TSource, TDestination>(
+            TSource source, 
+            TDestination destination, 
+            Action<IMappingOperationOptions<TSource, TDestination>> opts
+            )
         {
-            return _mapper.Map<TSource, TDestination>(source, destination, opts);
+            return _mapper.Map<TSource, TDestination>(
+                source, 
+                destination, 
+                opts
+                );
         }
 
-        public object Map(object source, Type sourceType, Type destinationType)
+        public object Map(
+            object source, 
+            Type sourceType, 
+            Type destinationType
+            )
         {
-            return _mapper.Map(source, sourceType, destinationType);
+            return _mapper.Map(
+                source, 
+                sourceType, 
+                destinationType
+                );
         }
 
-        public object Map(object source, Type sourceType, Type destinationType, Action<IMappingOperationOptions> opts)
+        public object Map(
+            object source, 
+            Type sourceType, 
+            Type destinationType, 
+            Action<IMappingOperationOptions> opts
+            )
         {
-            return _mapper.Map(source, sourceType, destinationType, opts);
+            return _mapper.Map(
+                source, 
+                sourceType, 
+                destinationType, 
+                opts
+                );
         }
 
-        public object Map(object source, object destination, Type sourceType, Type destinationType)
+        public object Map(
+            object source, 
+            object destination, 
+            Type sourceType, 
+            Type destinationType
+            )
         {
-            return _mapper.Map(source, destination, sourceType, destinationType);
+            return _mapper.Map(
+                source, 
+                destination, 
+                sourceType, 
+                destinationType
+                );
         }
 
-        public object Map(object source, object destination, Type sourceType, Type destinationType, Action<IMappingOperationOptions> opts)
+        public object Map(
+            object source, 
+            object destination, 
+            Type sourceType, 
+            Type destinationType, 
+            Action<IMappingOperationOptions> opts
+            )
         {
-            return _mapper.Map(source, destination, sourceType, destinationType, opts);
+            return _mapper.Map(
+                source, 
+                destination, 
+                sourceType, 
+                destinationType, 
+                opts
+                );
         }
     }
 }
