3b01d833c5898b61f8b8b103e7bfc933da819175
Tiaan de Klerk <tiaan.deklerk@gmail.com>
2016-02-19 10:46:00 +1300

    Added refactor TODO

--- src/Hermes/Internal.Engine/Configuration.ConfigurationSectionHandlers/EngineConfiguration.cs
+++ src/Hermes/Internal.Engine/Configuration.ConfigurationSectionHandlers/EngineConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿namespace Hermes.Internal.Engine.Configuration.ConfigurationSectionHandlers
 {
+    // TODO: Refactor EngineConfigurationSection class name to EngineConfiguration
     internal class EngineConfiguration :
         EngineConfigurationSection
     {
